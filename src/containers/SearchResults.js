3630e417f5daba49078e5263944375e6e54cde10
Dane Miller <unfaiyted@gmail.com>
2018-11-18 12:57:40 -0600

    fix item info height

--- src/containers/SearchResults.js
+++ src/containers/SearchResults.js
@@ -50,7 +50,7 @@ class SearchResults extends Component {
         <h2>Search Results</h2>
 
           <div className={'results-container'}>
-            {(typeof search.results == "object") ? search.results.map((item, i) => (
+            {(typeof search.results === "object") ? search.results.map((item, i) => (
 
               <CarouselItem key={i} item={item}/>
 
